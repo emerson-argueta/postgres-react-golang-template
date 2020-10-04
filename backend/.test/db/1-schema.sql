3a051ef334ecd22f5261bec49c4b74397406ee7d
emerson <emerson@pop-os.localdomain>
2020-10-03 22:01:33 -0700

    created schema for community-goal-tracker

--- backend/.test/db/1-schema.sql
+++ backend/.test/db/1-schema.sql
@@ -4,7 +4,10 @@
 DROP SCHEMA IF EXISTS IDENTITY CASCADE;
 Create SCHEMA IDENTITY;
 
--- Creating json schema used by identity subdomain tables
+-- Creating services json schema used by identity models
+-- object structure {[service_id_number:number]:{role:string}}
+-- ex: {1432:{role:administrator}}
+--    in this example id 1432 could be service with name communit_goal_tracker
 CREATE EXTENSION is_jsonb_valid;
 CREATE FUNCTION IDENTITY.SERVICES()
   RETURNS JSONB LANGUAGE SQL IMMUTABLE PARALLEL SAFE AS
@@ -12,11 +15,10 @@ CREATE FUNCTION IDENTITY.SERVICES()
   '{
     "type":"object",
     "patternProperties":{
-      "^.+$":{
+      "^[0-9]+$":{
         "type":"object",
         "properties":{
           "role":{"type":"string"},
-          "access":{"enum":["non-restricted","restricted"]}
         },
         "additionalProperties":false
       }
@@ -26,7 +28,7 @@ CREATE FUNCTION IDENTITY.SERVICES()
   $$
 ;
 
--- User
+-- User model
 CREATE EXTENSION IF NOT EXISTS pgcrypto;
 DROP TABLE IF EXISTS IDENTITY.USER;
 CREATE TABLE IDENTITY.USER (
@@ -36,12 +38,42 @@ CREATE TABLE IDENTITY.USER (
   SERVICES     JSONB CHECK (is_jsonb_valid(IDENTITY.SERVICES(),SERVICES))
 );
 
--- Creating schema for CHURCH_FUND_MANAGING subdomain
-DROP SCHEMA IF EXISTS CHURCH_FUND_MANAGING CASCADE;
-CREATE SCHEMA CHURCH_FUND_MANAGING;
+-- Service model
+DROP TABLE IF EXISTS IDENTITY.SERVICE;
+CREATE TABLE IDENTITY.SERVICE (
+	ID           SERIAL,
+	NAME         VARCHAR UNIQUE
+);
+-- Trigger to increment SERVICE ID
+CREATE OR REPLACE FUNCTION SERVICE_TRIGGER_FUNCTION()
+RETURNS TRIGGER AS $$
+DECLARE LAST_VAL INTEGER;
+DECLARE NEXT_ID INTEGER;
+BEGIN
+  SELECT last_value from IDENTITY.SERVICE_ID_SEQ
+  INTO LAST_VAL;
+  IF NOT NEW.ID = LAST_VAL THEN
+      SELECT NEXTVAL(PG_GET_SERIAL_SEQUENCE('IDENTITY.SERVICE', 'id')) AS NEW_ID
+      INTO NEXT_ID;
+      NEW.ID = NEXT_ID;
+  END IF;
+  RETURN NEW;
+END;
+$$ LANGUAGE 'plpgsql';
+CREATE TRIGGER SERVICE_TRIGGER
+BEFORE INSERT ON IDENTITY.SERVICE
+FOR EACH ROW
+EXECUTE PROCEDURE SERVICE_TRIGGER_FUNCTION();
 
--- Create all json schemas here for CHURCH_FUND_MANAGING application
-CREATE FUNCTION CHURCH_FUND_MANAGING.ADMINISTRATOR_CHURCHES()
+-- Creating schema for COMMUNITY_GOAL_TRACKER subdomain
+DROP SCHEMA IF EXISTS COMMUNITY_GOAL_TRACKER CASCADE;
+CREATE SCHEMA COMMUNITY_GOAL_TRACKER;
+
+-- Creating achievers json schema used by community_goal_tracker models
+-- object structure {[achiever_id_number:number]:{progress:string,messages:{[message_number:number]:string}}}
+-- ex: {1432:{progress:50,messages:{0:'layed down the concrete for the foundation'}}}
+--     in this example achiever_id_numer 1432 is the achiever id 
+CREATE FUNCTION COMMUNITY_GOAL_TRACKER.ACHIEVERS()
   RETURNS JSONB LANGUAGE SQL IMMUTABLE PARALLEL SAFE AS
   $$SELECT JSONB
   '{
@@ -50,8 +82,9 @@ CREATE FUNCTION CHURCH_FUND_MANAGING.ADMINISTRATOR_CHURCHES()
       "^[0-9]+$":{
         "type":"object",
         "properties":{
-          "role":{"enum":["creator","support"]},
-          "access":{"enum":["non-restricted","restricted"]}
+          "state":{"enum":["abandoned","inprogress","completed"]},
+          "progress":{"type":"integer"},
+          "messages":{"type":"object","patternProperties":{"^[0-9]+$":{"type":"string"}}}
         },
         "additionalProperties":false
       }
@@ -60,235 +93,59 @@ CREATE FUNCTION CHURCH_FUND_MANAGING.ADMINISTRATOR_CHURCHES()
   }'
   $$
 ;
-CREATE FUNCTION CHURCH_FUND_MANAGING.DONATOR_CHURCHES()
+
+-- Creating goals json schema used by community_goal_tracker models
+-- object structure {[goals_id_number]}
+-- ex: {[1432]}}
+--     in this example goals_id_numer 1432 could refer to goal with name bulid-house
+CREATE FUNCTION COMMUNITY_GOAL_TRACKER.GOALS()
   RETURNS JSONB LANGUAGE SQL IMMUTABLE PARALLEL SAFE AS
   $$SELECT JSONB
   '{
-    "type":"object",
-    "patternProperties":{
-      "^[0-9]+$":{
-        "type":"object",
-        "properties":{
-          "donationcount":{"type":"integer"},
-          "firstdonation":{"type":"string","format":"date"} 
-        },
-        "additionalProperties":true
-      }
-    },
-    "additionalProperties":false
-  }'
-  $$
-;
-CREATE FUNCTION CHURCH_FUND_MANAGING.DONATORS()
-  RETURNS JSONB LANGUAGE SQL IMMUTABLE PARALLEL SAFE AS
-  $$SELECT JSONB
-  '{
-    "type":"object",
-    "patternProperties":{
-      "^[0-9]+$":{
-        "type":"object",
-        "properties":{
-          "uuid":{"type":"string"}
-        },
-        "additionalProperties":true
-      }
-    },
-    "additionalProperties":false
-  }'
-  $$
-;
-CREATE FUNCTION CHURCH_FUND_MANAGING.ADMINISTRATORS()
-  RETURNS JSONB LANGUAGE SQL IMMUTABLE PARALLEL SAFE AS
-  $$SELECT JSONB
-  '{
-    "type":"object",
-    "patternProperties":{
-      "^.+$":{
-        "type":"object",
-        "properties":{
-          "role":{"enum":["creator","support"]},
-          "access":{"enum":["non-restricted","restricted"]}
-        },
-        "additionalProperties":false
-      }
-    },
-    "additionalProperties":false
-  }'
-  $$
-;
-CREATE FUNCTION CHURCH_FUND_MANAGING.ACCOUNTSTATEMENT()
-  RETURNS JSONB LANGUAGE SQL IMMUTABLE PARALLEL SAFE AS
-  $$SELECT JSONB
-  '{
-    "type":"object",
-    "properties":{
-      "closingbalance":{"type":"number"},
-      "date":{"type":"string","format":"date"}
-    },
-    "additionalProperties":false
-  }'
-  $$
-;
-CREATE FUNCTION CHURCH_FUND_MANAGING.DONATIONCATEGORIES()
-  RETURNS JSONB LANGUAGE SQL IMMUTABLE PARALLEL SAFE AS
-  $$SELECT JSONB
-  '{
-    "type":"object",
-    "patternProperties":{
-      "^.+$":{"type":"string"}
+    "type":"array",
+    "items":{
+      "type":"number"
     }
   }'
   $$
 ;
-CREATE FUNCTION CHURCH_FUND_MANAGING.DONATION()
-  RETURNS JSONB LANGUAGE SQL IMMUTABLE PARALLEL SAFE AS
-  $$SELECT JSONB
-  '{
-    "type":"object",
-    "properties":{
-      "donatorid":{"type":"number"},
-      "churchid":{"type":"number"},
-      "amount":{"type":"number"},
-      "type":{"type":"string"},
-      "currency":{"type":"string"},
-      "account":{"enum":["donator","church"]},
-      "category":{"type":"string"},
-      "details":{"type":"string"},
-      "date":{"type":"string","format":"date"}
-    },
-    "additionalProperties":false
-  }'
-  $$
-;
-CREATE FUNCTION CHURCH_FUND_MANAGING.SUBSCRIPTION()
-  RETURNS JSONB LANGUAGE SQL IMMUTABLE PARALLEL SAFE AS
-  $$SELECT JSONB
-  '{
-    "type":"object",
-    "properties":{
-      "freeusagelimitcount":{"type":"integer"},
-      "customeremail":{"type":"string"},
-      "type":{"type":"string"},
-      "paymentgateway":{"type":"object"}
-    },
-    "additionalProperties":false
-  }'
-  $$
-;
--- Create all tables for CHURCH_FUND_MANAGING application
--- Admin 
-DROP TABLE IF EXISTS CHURCH_FUND_MANAGING.ADMINISTRATOR;
-CREATE TABLE CHURCH_FUND_MANAGING.ADMINISTRATOR (
-  --ID           SERIAL,
+
+-- Create all tables for COMMUNITY_GOAL_TRACKER domain
+-- Achiever 
+DROP TABLE IF EXISTS COMMUNITY_GOAL_TRACKER.ACHIEVER;
+CREATE TABLE COMMUNITY_GOAL_TRACKER.ACHIEVER (
 	UUID         VARCHAR UNIQUE,
 	FIRSTNAME    VARCHAR,
 	LASTNAME     VARCHAR,
   ADDRESS      VARCHAR,
   PHONE        VARCHAR,
-  CHURCHES     JSONB CHECK (is_jsonb_valid(CHURCH_FUND_MANAGING.ADMINISTRATOR_CHURCHES(),CHURCHES)),
-  SUBSCRIPTION JSONB CHECK (is_jsonb_valid(CHURCH_FUND_MANAGING.SUBSCRIPTION(),SUBSCRIPTION))
+  GOALS        JSONB CHECK (is_jsonb_valid(COMMUNITY_GOAL_TRACKER.GOALS(),GOALS))
 );
--- Trigger to increment ADMIN ID
--- CREATE OR REPLACE FUNCTION ADMINISTRATOR_TRIGGER_FUNCTION()
--- RETURNS TRIGGER AS $$
--- DECLARE LAST_VAL INTEGER;
--- DECLARE NEXT_ID INTEGER;
--- BEGIN
---   SELECT last_value from CHURCH_FUND_MANAGING.ADMINISTRATOR_ID_SEQ
---   INTO LAST_VAL;
---   IF NOT NEW.ID = LAST_VAL THEN
---       SELECT NEXTVAL(PG_GET_SERIAL_SEQUENCE('CHURCH_FUND_MANAGING.ADMINISTRATOR', 'id')) AS NEW_ID
---       INTO NEXT_ID;
---       NEW.ID = NEXT_ID;
---   END IF;
---   RETURN NEW;
--- END;
--- $$ LANGUAGE 'plpgsql';
--- CREATE TRIGGER ADMINISTRATOR_TRIGGER
--- BEFORE INSERT ON CHURCH_FUND_MANAGING.ADMINISTRATOR
--- FOR EACH ROW
--- EXECUTE PROCEDURE ADMINISTRATOR_TRIGGER_FUNCTION();
 
--- Church 
-DROP TABLE IF EXISTS CHURCH_FUND_MANAGING.CHURCH;
-CREATE TABLE CHURCH_FUND_MANAGING.CHURCH (
+-- Goal 
+DROP TABLE IF EXISTS COMMUNITY_GOAL_TRACKER.GOAL;
+CREATE TABLE COMMUNITY_GOAL_TRACKER.GOAL (
   ID                SERIAL,
-	TYPE              VARCHAR,
-  NAME              VARCHAR,
-  ADDRESS           VARCHAR,
-  PHONE             VARCHAR,
-  EMAIL             VARCHAR UNIQUE,
-	PASSWORD          VARCHAR,
-  ADMINISTRATORS    JSONB CHECK (is_jsonb_valid(CHURCH_FUND_MANAGING.ADMINISTRATORS(),ADMINISTRATORS)),
-  DONATORS          JSONB CHECK (is_jsonb_valid(CHURCH_FUND_MANAGING.DONATORS(),DONATORS)),
-  ACCOUNTSTATEMENT  JSONB CHECK (is_jsonb_valid(CHURCH_FUND_MANAGING.ACCOUNTSTATEMENT(),ACCOUNTSTATEMENT)),
-  DONATIONCATEGORIES JSONB CHECK (is_jsonb_valid(CHURCH_FUND_MANAGING.DONATIONCATEGORIES(),DONATIONCATEGORIES))
+  NAME              VARCHAR UNIQUE,
+  ACHIEVERS    JSONB CHECK (is_jsonb_valid(COMMUNITY_GOAL_TRACKER.ACHIEVERS(),ACHIEVERS))
 );
 -- Trigger to increment CHURCH ID
-CREATE OR REPLACE FUNCTION CHURCH_TRIGGER_FUNCTION()
+CREATE OR REPLACE FUNCTION GOAL_TRIGGER_FUNCTION()
 RETURNS TRIGGER AS $$
 DECLARE LAST_VAL INTEGER;
 DECLARE NEXT_ID INTEGER;
 BEGIN
-  SELECT last_value from CHURCH_FUND_MANAGING.CHURCH_ID_SEQ
+  SELECT last_value from COMMUNITY_GOAL_TRACKER.GOAL_ID_SEQ
   INTO LAST_VAL;
   IF NOT NEW.ID = LAST_VAL THEN
-      SELECT NEXTVAL(PG_GET_SERIAL_SEQUENCE('CHURCH_FUND_MANAGING.CHURCH', 'id')) AS NEW_ID
+      SELECT NEXTVAL(PG_GET_SERIAL_SEQUENCE('COMMUNITY_GOAL_TRACKER.GOAL', 'id')) AS NEW_ID
       INTO NEXT_ID;
       NEW.ID = NEXT_ID;
   END IF;
   RETURN NEW;
 END;
 $$ LANGUAGE 'plpgsql';
-CREATE TRIGGER CHURCH_TRIGGER
-BEFORE INSERT ON CHURCH_FUND_MANAGING.CHURCH
+CREATE TRIGGER GOAL_TRIGGER
+BEFORE INSERT ON COMMUNITY_GOAL_TRACKER.GOAL
 FOR EACH ROW
-EXECUTE PROCEDURE CHURCH_TRIGGER_FUNCTION();
-
--- Donator 
-DROP TABLE IF EXISTS CHURCH_FUND_MANAGING.DONATOR;
-CREATE TABLE CHURCH_FUND_MANAGING.DONATOR (
-  ID                SERIAL,
-	UUID              VARCHAR,
-	FIRSTNAME         VARCHAR,
-	LASTNAME          VARCHAR,
-  EMAIL             VARCHAR UNIQUE,
-  ADDRESS           VARCHAR,
-  PHONE             VARCHAR,
-  CHURCHES          JSONB CHECK (is_jsonb_valid(CHURCH_FUND_MANAGING.DONATOR_CHURCHES(),CHURCHES)),
-  ACCOUNTSTATEMENT  JSONB CHECK (is_jsonb_valid(CHURCH_FUND_MANAGING.ACCOUNTSTATEMENT(),ACCOUNTSTATEMENT))
-
-);
--- Trigger to increment DONATOR ID
-CREATE OR REPLACE FUNCTION DONATOR_TRIGGER_FUNCTION()
-RETURNS TRIGGER AS $$
-DECLARE LAST_VAL INTEGER;
-DECLARE NEXT_ID INTEGER;
-BEGIN
-  SELECT last_value from CHURCH_FUND_MANAGING.DONATOR_ID_SEQ
-  INTO LAST_VAL;
-  IF NOT NEW.ID = LAST_VAL THEN
-      SELECT NEXTVAL(PG_GET_SERIAL_SEQUENCE('CHURCH_FUND_MANAGING.DONATOR', 'id')) AS NEW_ID
-      INTO NEXT_ID;
-      NEW.ID = NEXT_ID;
-  END IF;
-  RETURN NEW;
-END;
-$$ LANGUAGE 'plpgsql';
-CREATE TRIGGER DONATOR_TRIGGER
-BEFORE INSERT ON CHURCH_FUND_MANAGING.DONATOR
-FOR EACH ROW
-EXECUTE PROCEDURE DONATOR_TRIGGER_FUNCTION();
-
--- TRANSACTION 
---     Partition by CHURCHID
-CREATE TYPE TRANSACTIONTYPE AS ENUM ('credit', 'debit');
-CREATE TABLE CHURCH_FUND_MANAGING.TRANSACTION(
-	DONATORID   INT,
-  CHURCHID    INT,                     
-	AMOUNT      NUMERIC(15,6),
-  TYPE        TRANSACTIONTYPE, 
-	Donation    JSONB CHECK (is_jsonb_valid(CHURCH_FUND_MANAGING.DONATION(),Donation)),
-	CREATEDAT   DATE,
-  UPDATEDAT   DATE
-);
+EXECUTE PROCEDURE GOAL_TRIGGER_FUNCTION();
